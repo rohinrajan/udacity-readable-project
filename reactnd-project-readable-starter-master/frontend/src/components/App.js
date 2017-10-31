69d3744316e523dd263af92906d039433e118503
Rohin Rajan <rajan.rohin@gmail.com>
2017-10-30 20:43:56 -0700

    latest changes and folder structure

---
+++ reactnd-project-readable-starter-master/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is udacity Project 2</p>
+      </div>
+    );
+  }
+}
+
+export default App;
